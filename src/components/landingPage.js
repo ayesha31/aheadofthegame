cdf59c74e0dd864cb90325db577612bb76333761
Ayesha Malshani <ayesha@192-168-1-101.tpgi.com.au>
2020-10-25 10:28:57 +1100

    Fix styling

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -15,16 +15,12 @@ class LandingPage extends React.Component {
                     <div className="App-container">
                         <h2 className="heading">are you a...?</h2>
 
-//                         <form>
-//                             <Link to="/parent" parent>parent</Link> <br />
-//                             <Link to="/coachSession" parent>coach</Link><br />
-//                             <Link to="/player" parent>player</Link><br />
-//                         </form>
+                        <form>
+                            <Link to={{ pathname: '/login', user: 'parent' }} ><button className="LPButton">parent</button></Link> <br /><br />
+                            <Link to={{ pathname: '/login', user: 'coach' }} parent><button className="LPButton">coach</button></Link><br /><br />
+                            <Link to={{ pathname: '/login', user: 'player' }} parent><button className="LPButton">player</button></Link><br /><br />
 
-                        <Link to={{ pathname: '/login', user: 'parent' }} ><button className="LPButton">parent</button></Link> <br /><br />
-                        <Link to={{ pathname: '/login', user: 'coach' }} parent><button className="LPButton">coach</button></Link><br /><br />
-                        <Link to={{ pathname: '/login', user: 'player' }} parent><button className="LPButton">player</button></Link><br /><br />
-
+                        </form>
                     </div>
                 </header>
             </div>
