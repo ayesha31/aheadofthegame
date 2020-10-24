314289277f7e54de2565556472a923dd205cda74
Sunntaill <elizabethtrejure@hotmail.com>
2020-10-25 00:40:32 +1100

    Add content for about page

--- src/components/about.js
+++ src/components/about.js
@@ -14,12 +14,17 @@ class About extends React.Component {
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <ul className="header"> About 
-            <li><a href="/">Home</a></li>
+            <li><a href="/landingPage">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/contactUs">Contact Us</a></li>
             </ul>
             </header>
                 <div className="content">
+                    <h1> About </h1>
+                       <p> Ahead of the Game uses sport to teach athletes, parents and coaches how to talk about mental health. 
+                           The program reviews how to recognise mental health issues, what to do and when to get help. 
+                           Athletes learn how to build resilience and overcome challenges in sport and life.
+                      </p>
                 
         </div>
         
