48153c83169d467dcfe8116dce1dd2b6e75632f9
Ayesha Malshani <ayesha@192-168-1-101.tpgi.com.au>
2020-10-24 20:49:08 +1100

    Completed landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/Mo_Black.jpg';
 import './App.css';
 import Login from './components/login.js';
 import Parent from './components/parent.js';
@@ -13,6 +13,12 @@ function App() {
     //<Login/>
     <BrowserRouter>
       <div>
+        <ul>
+          <li className="menuItemLeft"><img src={logo} className="App-logo" alt="logo" /></li>
+          <li className="menuItemCentre">Ahead of the game</li>
+          <li className="menuItemRight"><a href="#contact">Contact</a></li>
+          <li className="menuItemRight"><a href="#about">About</a></li>
+        </ul>
         <Route path="/" exact component={LandingPage} />
         <Route path="/login" exact component={Login} />
         <Route path="/parent" component={Parent} />
--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -15,9 +15,9 @@ class LandingPage extends React.Component {
                     <div className="App-container">
                         <h2 className="heading">are you a...?</h2>
                         <form>
-                            <Link to="/parent" parent>parent</Link> <br />
-                            <Link to="/coach" parent>coach</Link><br />
-                            <Link to="/player" parent>player</Link><br />
+                            <Link to="/parent" parent><button className="LPButton">parent</button></Link> <br /><br />
+                            <Link to="/coach" parent><button className="LPButton">coach</button></Link><br /><br />
+                            <Link to="/player" parent><button className="LPButton">player</button></Link><br /><br />
                         </form>
                     </div>
                 </header>
