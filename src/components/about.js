d52041765dc5c338b3f7bf0d5d9b20326ab4dfb0
Sunntaill <elizabethtrejure@hotmail.com>
2020-10-24 19:56:26 +1100

    add About Us and Contact Pages

---
+++ src/components/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../logo.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+class About extends React.Component {
+    render() {
+        return (<div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <ul className="header"> About 
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contactUs">Contact Us</a></li>
+            </ul>
+            </header>
+                <div className="content">
+        </div>
+        
+    </div>
+);
+}
+}
+export default About;
