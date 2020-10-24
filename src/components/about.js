6dcc0917d7ba4b0c86ece536a066fe7ba53292bd
Ayesha Malshani <ayesha@192-168-1-101.tpgi.com.au>
2020-10-25 00:58:40 +1100

    Fix styling issues from aboutUs

--- src/components/about.js
+++ src/components/about.js
@@ -10,25 +10,18 @@ import {
 
 class About extends React.Component {
     render() {
-        return (<div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <ul className="header"> About
-            <li><a href="/landingPage">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contactUs">Contact Us</a></li>
-                </ul>
-            </header>
-            <div className="content">
-                <h1> About </h1>
-                <p> Ahead of the Game uses sport to teach athletes, parents and coaches how to talk about mental health.
-                The program reviews how to recognise mental health issues, what to do and when to get help.
-                Athletes learn how to build resilience and overcome challenges in sport and life.
+        return (
+            <div className="App">
+                <div className="AboutUsContent">
+                    <h1> About </h1>
+                    <p> Ahead of the Game uses sport to teach athletes, parents and coaches how to talk about mental health.
+                    The program reviews how to recognise mental health issues, what to do and when to get help.
+                    Athletes learn how to build resilience and overcome challenges in sport and life.
                       </p>
 
-            </div>
+                </div>
 
-        </div>
+            </div>
         );
     }
 }
