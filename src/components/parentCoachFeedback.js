a768fe6451b6a8a69d55658da3893d487425dec5
Ayesha Malshani <ayesha@192-168-1-101.tpgi.com.au>
2020-10-24 18:23:52 +1100

    Add components

---
+++ src/components/parentCoachFeedback.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Link } from "react-router-dom";
+
+class ParentCoachFeedback extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Child's Full Name: <input type="text"
+                                value={this.state.value} onChange={this.handleChange}
+                                placeholder="Full Name (no nicknames)" required />
+                        </label>
+                    </form>
+                    {/* <button>Submit</button> */}
+                    <input type="submit" value="Submit" />
+                    <Link to="/"><button>Back</button></Link>
+                </header>
+            </div>
+        );
+    }
+}
+export default ParentCoachFeedback;
