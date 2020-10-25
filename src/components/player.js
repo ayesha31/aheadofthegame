3b22562670fd724e158b4dc30fe8512156fb9b6d
deannanicdao <deanna.nicdao@gmail.com>
2020-10-25 11:26:05 +1000

    Moved avatar to its own page

--- src/components/player.js
+++ src/components/player.js
@@ -1,48 +1,26 @@
-import React from 'react'
-import logo from '../logo.svg';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import ImagePicker from 'react-image-picker'
-import 'react-image-picker/dist/index.css'
-
-//import images from local
-import img1 from '../images/blue_avatar_plain.png'
-import img2 from '../images/orange_avatar_plain.png'
-import img3 from '../images/green_avatar_plain.png'
-import img4 from '../images/purple_avatar_plain.png'
-import img5 from '../images/yellow_avatar_plain.png'
-
-
-const imageList = [img1, img2, img3, img4, img5]
+import React from 'react';
+import { Link } from "react-router-dom";
 
 class Player extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      image: null
+      description: 'Enter your answer here.'
+    };
+  }
+    render() {
+        return (
+          // render html? navbar here
+          <div className="App">
+            <div className="App-form">
+              <form className="coach-form">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et tortor a magna ullamcorper cursus?</p>
+                <textarea className="coach-text" value={this.state.description} />
+                <input id="button" type='submit' value="CONTINUE" />
+              </form>
+            </div>
+          </div>
+        );
     }
-    this.onPick = this.onPick.bind(this)
-  }
-
-  onPick(image) {
-    this.setState({image})
-  }
-
-  render() {
-    return (
-      <div className="avatarSelection">
-        <ImagePicker 
-          images={imageList.map((image, i) => ({src: image, value: i}))}
-          onPick={this.onPick}
-        />
-        <button type="button" onClick={() => console.log(this.state.image)}>OK</button>
-      </div>
-    )
-  }
 }
-
 export default Player;
